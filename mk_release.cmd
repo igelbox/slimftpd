dc2d02f763458c1813cd8095e4065d1fdec35449
Mike Akiba <mike2718@gmail.com>
2014-10-25 00:11:31 +0800

    Use DIST variable and not need mk_release.sh

--- mk_release.cmd
+++ mk_release.cmd
@@ -1,18 +1,21 @@
 @echo off
 
+set DIST=dist
+set NAME=SlimFTPd
+
 > VERSION.txt awk -F\" "/ProductVersion/ {print $4}" src\SlimFTPd.rc
 < VERSION.txt set /p VERSION=
 
-if exist dist\nul rmdir /s /q dist
-mkdir dist\SlimFTPd
-cd dist
-del /f /q SlimFTPd\*
-copy /y ..\SlimFTPd.exe SlimFTPd\
-copy /y ..\LICENSE.txt SlimFTPd\
-copy /y ..\slimftpd.conf.example SlimFTPd\
->  SlimFTPd\slimftpd.conf echo ^<User "anonymous"^>
->> SlimFTPd\slimftpd.conf echo ^    Mount / .
->> SlimFTPd\slimftpd.conf echo ^    Allow / List Read
->> SlimFTPd\slimftpd.conf echo ^</User^>
-zip -r SlimFTPd-%VERSION%.zip SlimFTPd
+if exist %DIST%\nul rmdir /s /q %DIST%
+mkdir %DIST%\%NAME%
+cd %DIST%
+del /f /q %NAME%\*
+copy /y ..\SlimFTPd.exe %NAME%
+copy /y ..\LICENSE.txt %NAME%
+copy /y ..\slimftpd.conf.example %NAME%
+>  %NAME%\slimftpd.conf echo ^<User "anonymous"^>
+>> %NAME%\slimftpd.conf echo ^    Mount / .
+>> %NAME%\slimftpd.conf echo ^    Allow / List Read
+>> %NAME%\slimftpd.conf echo ^</User^>
+zip -r SlimFTPd-%VERSION%.zip %NAME%
 pause
